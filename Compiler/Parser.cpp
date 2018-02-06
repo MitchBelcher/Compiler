8ed2f7e3acb48585bca69ee6c576917badc2b582
OriginSaint <Guitarfanatic19023@Gmail.com>
2018-02-06 11:11:15 -0500

    Filling out Parser functions

--- Compiler/Parser.cpp
+++ Compiler/Parser.cpp
@@ -95,73 +95,135 @@ void Parser::ProgramBody() {
 	}
 }
 
-
+// Declare
 void Parser::Declare() {
 
+	// CHECK FOR GLOBAL
+	if (tempToken.t_type == GLOBAL) {
+		tempToken = inputScanner.tokenScan(); // Get next token, ADDED GLOBAL TO TREE
+
+		// CHECK FOR PROCEDURE
+		if (tempToken.t_type == PROCEDURE) {
+			ProcDeclare(); // Run procedure declaration procedure
+		}
+
+		// Check if type is a variable
+		else if (tempToken.t_type == INTEGER || tempToken.t_type == CHAR || tempToken.t_type == STRING || tempToken.t_type == FLOAT || tempToken.t_type == BOOL) {
+			VarDeclare(); // Run variable declaration procedure
+		}
+	}
+
+	// CHECK FOR PROCEDURE
+	else if (tempToken.t_type == PROCEDURE) {
+		ProcDeclare(); // Run procedure declaration procedure
+	}
+
+	// Check if type is a variable
+	else if (tempToken.t_type == INTEGER || tempToken.t_type == CHAR || tempToken.t_type == STRING || tempToken.t_type == FLOAT || tempToken.t_type == BOOL) {
+		VarDeclare(); // Run variable declaration procedure
+	}
 }
 
-
+// Statement
 void Parser::Statement() {
 
+	// Assign statement/Procedure 
+	if (tempToken.t_type == IDENTIFIER) {
+		/////////////////////////////////
+		// TODO /////////////////////////
+		/////////////////////////////////
+	}
+
+	// If conditional start
+	else if (tempToken.t_type == IF) {
+		If(); // Run if procedure
+	}
+
+	// For loop start
+	else if (tempToken.t_type == FOR) {
+		Loop(); // Run loop procedure
+	}
+
+	// Return statement
+	else if (tempToken.t_type == RETURN) {
+		Return(); // Run return procedure
+	}
+	else {
+		// ERROR, NO VALID STATEMENT PRESENT
+	}
 }
 
-
+// Procedure Declaration
 void Parser::ProcDeclare() {
-
+	ProcHead(); // Run procedure head procedure
+	ProcBody(); // Run procedure body procedure
 }
 
-
+// Variable Declaration
 void Parser::VarDeclare() {
+	TypeMark();
+	Ident();
 
+	// Check for left bracket
+	if (tempToken.t_type == BRACKBEGIN) {
+		tempToken = inputScanner.tokenScan(); // Get next token, ADD LEFT BRACKET TO TREE
+		Number(); // Run number procedure
+
+		// Check for colon
+		if (tempToken.t_type == COLON) {
+			tempToken = inputScanner.tokenScan(); // Get next token
+			Number();
+
+			// Check for right bracket
+			if (tempToken.t_type == BRACKEND) {
+
+			}
+		}
+	}
 }
 
-
+// Procedure Head
 void Parser::ProcHead() {
 
 }
 
-
+// Procedure Body
 void Parser::ProcBody() {
 
 }
 
-
+// Parameter List
 void Parser::ParamList() {
 
 }
 
-
+// Parameter
 void Parser::Param() {
 
 }
 
-
+// Type Mark
 void Parser::TypeMark() {
 
 }
 
-
+// Assign
 void Parser::Assign() {
 
 }
 
-
+// If
 void Parser::If() {
 
 }
 
-
+// Loop
 void Parser::Loop() {
 
 }
 
-
+// Return
 void Parser::Return() {
-
-}
-
-
-void Parser::ProcCall() {
 
 }
 
